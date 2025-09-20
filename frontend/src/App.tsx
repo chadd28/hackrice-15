6ff0f42ea33ec3cf9012d0069f4352923db9cbae
Chad <chaddiao0@gmail.com>
2025-09-19 21:22:46 -0500

    initial

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import LandingPage from './pages/LandingPage'
 import SampleLandingPage from './pages/SampleLandingPage'
 import TestTTSPage from './pages/TestTTSPage'
 
 function App(): React.ReactElement {
   return (
     <Routes>
+      <Route path="/" element={<LandingPage />} />
       <Route path="/sample-landing" element={<SampleLandingPage />} />
       <Route path="/test-tts" element={<TestTTSPage />} />
     </Routes>
